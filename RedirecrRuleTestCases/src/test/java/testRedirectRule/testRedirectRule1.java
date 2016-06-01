d33a95f917cbe2275a967e155b03af0dec27394e
jerry1101 <jerry110160@gmail.com>
2016-06-01 15:09:57 -0400

    add test1

--- RedirecrRuleTestCases/src/test/java/testRedirectRule/testRedirectRule1.java
+++ RedirecrRuleTestCases/src/test/java/testRedirectRule/testRedirectRule1.java
@@ -1,5 +1,16 @@
 package testRedirectRule;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
@@ -8,7 +19,48 @@ import org.testng.annotations.AfterTest;
 
 public class testRedirectRule1 {
   @Test
-  public void f() {
+  public void case1() {
+	  CloseableHttpClient httpclient = HttpClients.createDefault();
+      try {
+          HttpGet httpget = new HttpGet("http://www.totalwine.com/");
+
+          System.out.println("Executing request " + httpget.getRequestLine());
+
+          // Create a custom response handler
+          ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+
+              public String handleResponse(
+                      final HttpResponse response) throws ClientProtocolException, IOException {
+                  int status = response.getStatusLine().getStatusCode();
+                  if (status >= 200 && status < 300) {
+                      HttpEntity entity = response.getEntity();
+                      return entity != null ? EntityUtils.toString(entity) : null;
+                  } else {
+                      throw new ClientProtocolException("Unexpected response status: " + status);
+                  }
+              }
+
+          };
+          String responseBody = null;
+		try {
+			responseBody = httpclient.execute(httpget, responseHandler);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+          System.out.println("----------------------------------------");
+          System.out.println(responseBody);
+      } finally {
+          try {
+			httpclient.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+      }
   }
   @BeforeMethod
   public void beforeMethod() {
