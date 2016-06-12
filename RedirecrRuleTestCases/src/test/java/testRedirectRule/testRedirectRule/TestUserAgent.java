a4e5c71f342a70425880ccd4eead32327a582701
Jerry Hung <jerry110160@gmail.com>
2016-06-12 15:49:45 -0400

    New Branch

--- RedirecrRuleTestCases/src/test/java/testRedirectRule/testRedirectRule/TestUserAgent.java
+++ RedirecrRuleTestCases/src/test/java/testRedirectRule/testRedirectRule/TestUserAgent.java
@@ -63,6 +63,7 @@ public class TestUserAgent {
 		 
 		 } catch (URISyntaxException e) {
 				// TODO Auto-generated catch block
+			 System.out.println("tese");
 				e.printStackTrace();
 			}finally {
 		     try {
